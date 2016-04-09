896c6967d818195ed8dad7669f92a1eb8277b77a
oreqizer <oreqizer@gmail.com>
2016-04-09 23:45:31 +0200

    INIT

---
+++ Iterables/generators.py
@@ -0,0 +1,28 @@
+# Genexps (generator expressions):
+# generated lazily - not kept in memory
+# as opposed to listcomps (they generate list and feed the constructor)
+
+import array
+
+symbols = '@#$%^&'
+
+
+def tuples():
+    print(tuple(ord(symbol) for symbol in symbols))
+
+
+def arrays():
+    print(array.array('I', (ord(symbol) for symbol in symbols)))
+
+
+def cartesian():
+    colors = ['black', 'red']
+    sizes = ['S', 'M', 'L']
+    for shirt in ('%s %s' % (size, color) for color in colors for size in sizes):
+        print(shirt)
+
+
+if __name__ == '__main__':
+    tuples()
+    arrays()
+    cartesian()
---
+++ Iterables/listcomps.py
@@ -0,0 +1,43 @@
+# Listcomps (list comprehensions):
+# used for producing new lists
+# not good for stuff like cartesian product and such
+# as they are kept in memory as a whole
+
+symbols = '@#$%^&'
+
+
+def potato():
+    codes = []
+    for symbol in symbols:
+        codes.append(ord(symbol))
+
+    print('Potato: {}'.format(codes))
+
+
+def listcomp():
+    codes = [ord(symbol) for symbol in symbols]
+    print('Listcomp: {}'.format(codes))
+
+
+def cartesian():
+    ranks = ['A', 'Q', 'K']
+    suits = ['spades', 'diamonds', 'clubs', 'hearts']
+    cards = [(rank, suit) for suit in suits for rank in ranks]
+    print('Cartesian product: {}'.format(cards))
+
+
+# Python 2:
+# variables were getting overwritten in listcomp
+def variable():
+    x = 'Outside'
+    print('x before listcomp: {}'.format(x))
+    codes = [ord(x) for x in 'ABC']
+    print('x after listcomp: {}'.format(x))
+    print('Listcomp in "variables": {}'.format(codes))
+
+
+if __name__ == '__main__':
+    potato()
+    listcomp()
+    cartesian()
+    variable()
